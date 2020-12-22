7fb8a4c4879fb148e607a627a98d0111e5a942a3
Chandler Brtek <chandler.brtek@aviture.us.com>
2020-12-21 18:09:43 -0600

    made a bit of progress on stack turns out that pytest does weird stuff with instantiation of classes, gotta figure that out soon

--- python_data_structures/stack.py
+++ python_data_structures/stack.py
@@ -1,3 +1,15 @@
 """Main module."""
 class Stack:
-    stub = 0
+    stack = []
+
+    def isEmpty(self) -> bool:
+        return len(self.stack) == 0
+
+    def push(self, value: any) -> None:
+       self.stack.append(value)
+    
+    def peek(self) -> any:
+        return self.stack[-1]
+    
+    def pop(self) -> any:
+        return self.stack.pop()
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -7,5 +7,34 @@ from click.testing import CliRunner
 from python_data_structures.stack import Stack
 
 def test_stack_is_empty_when_initialized():
-    mystack = Stack()
-    assert mystack.isEmpty()
+    stack = Stack()
+    assert stack.isEmpty()
+
+def test_stack_is_not_empty_after_push():
+    stack = Stack()
+    stack.push(1)
+    assert not stack.isEmpty()
+
+def test_stack_peek_returns_most_recent_push():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    assert stack.peek() == 2
+
+def test_stack_pop_gets_most_recent_push():
+    stack = Stack()
+    stack.push(123)
+    stack.push(456)
+    assert stack.pop() == 456
+
+def test_stack_pop_shows_second_most_recent_on_peek():
+    stack = Stack()
+    stack.push(123)
+    stack.push(456)
+    stack.pop()
+    assert stack.peek() == 123
+
+def test_stack_peek_on_empty_stack_throws_error():
+    stack = Stack()
+
+    assert stack.peek() == 123
